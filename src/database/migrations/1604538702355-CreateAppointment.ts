9c94f926d5fa7d6dc49bf19082da28e52db5befc
Luis Paulo Ruda <luispauloruda@gmail.com>
2020-11-05 22:55:57 -0300

    Relacionamento nos models

--- src/database/migrations/1604538702355-CreateAppointment.ts
+++ src/database/migrations/1604538702355-CreateAppointment.ts
@@ -9,7 +9,7 @@ export default class CreateAppointment1604538702355
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1604625123785-CreateUsers.ts
+++ src/database/migrations/1604625123785-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1604625123785 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
