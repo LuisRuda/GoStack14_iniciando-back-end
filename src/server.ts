9db6afa5bfc073ad2ba5421a7eb17ee1bb35f743
Luis Paulo Ruda <luispauloruda@gmail.com>
2020-11-06 23:10:32 -0300

    Lidando com erros

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 
-import express from 'express';
-import routes from './routes';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
+import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -13,7 +15,21 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
