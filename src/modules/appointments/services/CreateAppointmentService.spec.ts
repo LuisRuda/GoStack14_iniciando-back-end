7d943f6820423c497f49d45dec2760726cb9dbf5
Luis Paulo Ruda <luispauloruda@gmail.com>
2021-01-09 23:03:31 -0300

    Criação do agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,6 +17,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '21321',
       provider_id: '21321412',
     });
 
@@ -29,12 +30,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '21321',
       provider_id: '21321412',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '21321',
         provider_id: '21321412',
       }),
     ).rejects.toBeInstanceOf(AppError);
