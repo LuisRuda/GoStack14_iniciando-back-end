1fdde7ba6f533674fcad3d21695e16a3b1bc3cae
Luis Paulo Ruda <luispauloruda@gmail.com>
2021-01-12 22:03:51 -0300

    Refatorando testes

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -1,7 +1,6 @@
 import Redis, { Redis as RedisClient } from 'ioredis';
 
 import cacheConfig from '@config/cache';
-import { da } from 'date-fns/locale';
 import ICacheProvider from '../models/ICacheProvider';
 
 export default class RedisCacheProvider implements ICacheProvider {
